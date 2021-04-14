1d44fbe0fb2169254ba0a00a02dd42f5123620b7
shadyaziza <shadyaziza@gmail.com>
2021-04-14 13:56:44 +0200

    Wire graphql to todo all

--- lib/data/todo_fetch.dart
+++ lib/data/todo_fetch.dart
@@ -8,4 +8,20 @@ class TodoFetch {
     is_completed
   }
 }""";
+  static String fetchActive = """query getActiveTodos{
+  todos(where: {is_public: {_eq: false}, is_completed: {_eq: false}}, order_by: {created_at: desc}) {
+    __typename
+    is_completed
+    id
+    title
+  }
+  }""";
+  static String fetchCompleted = """query getCompletedTodos{
+  todos(where: {is_public: {_eq: false}, is_completed: {_eq: true}}, order_by: {created_at: desc}) {
+    __typename
+    is_completed
+    id
+    title
+  }
+  }""";
 }
--- lib/screens/tabs/todos/all.dart
+++ lib/screens/tabs/todos/all.dart
@@ -1,7 +1,13 @@
 import 'package:app_boilerplate/components/add_task.dart';
 import 'package:app_boilerplate/components/todo_item_tile.dart';
+import 'package:app_boilerplate/data/todo_fetch.dart';
 import 'package:app_boilerplate/data/todo_list.dart';
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+import '../../../components/todo_item_tile.dart';
+import '../../../data/todo_list.dart';
+import '../../../model/todo_item.dart';
 
 class All extends StatefulWidget {
   All({Key key}) : super(key: key);
@@ -16,6 +22,7 @@ class _AllState extends State<All> {
     super.initState();
   }
 
+  VoidCallback refetchQuery;
   @override
   Widget build(BuildContext context) {
     print("All tab");
@@ -27,20 +34,43 @@ class _AllState extends State<All> {
           },
         ),
         Expanded(
-          child: ListView.builder(
-            itemCount: todoList.list.length,
-            itemBuilder: (context, index) {
-              return TodoItemTile(
-                item: todoList.list[index],
-                delete: () {
-                  setState(() {
-                    todoList.removeTodo(todoList.list[index].id);
-                  });
-                },
-                toggleIsCompleted: () {
-                  setState(() {
-                    todoList.toggleList(todoList.list[index].id);
-                  });
+          child: Query(
+            options: QueryOptions(document: TodoFetch.fetchAll),
+            builder: (QueryResult result,
+                {VoidCallback refetch, FetchMore fetchMore}) {
+              refetchQuery = refetch;
+              if (result.hasErrors) {
+                final snackBar = SnackBar(
+                  backgroundColor: Colors.redAccent,
+                  content: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [Text("Error"), Icon(Icons.error)],
+                  ),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                return Container();
+              }
+              if (result.loading) {
+                final snackBar = SnackBar(
+                  content: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [Text("Loading"), CircularProgressIndicator()],
+                  ),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                return Container();
+              }
+              final List<LazyCacheMap> todos =
+                  (result.data['todos'] as List<dynamic>).cast<LazyCacheMap>();
+              return ListView.builder(
+                itemCount: todos.length,
+                itemBuilder: (context, index) {
+                  dynamic responseData = todos[index];
+                  return TodoItemTile(
+                      item: TodoItem.fromElements(responseData["id"],
+                          responseData['title'], responseData['is_completed']),
+                      delete: () {},
+                      toggleIsCompleted: () {});
                 },
               );
             },
